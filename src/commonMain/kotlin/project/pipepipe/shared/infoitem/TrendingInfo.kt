488b67f3f68d269b618aed85cc9acff35e3a5914
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2025-10-18 17:23:41 +0800

    feat: trending UI

--- src/commonMain/kotlin/project/pipepipe/shared/infoitem/TrendingInfo.kt
+++ src/commonMain/kotlin/project/pipepipe/shared/infoitem/TrendingInfo.kt
@@ -1,3 +1,6 @@
 package project.pipepipe.shared.infoitem
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class TrendingInfo(val url: String, val serviceId: String, val name: String): Info
